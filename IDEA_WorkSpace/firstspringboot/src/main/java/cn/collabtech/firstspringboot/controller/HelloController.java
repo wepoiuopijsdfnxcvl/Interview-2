ff14bf70af13a0a48ad3768d91f811a0172aef5a
DarrickAZ <zhwwhx@126.com>
2019-01-22 09:04:50 +0800

    ###update.

---
+++ IDEA_WorkSpace/firstspringboot/src/main/java/cn/collabtech/firstspringboot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cn.collabtech.firstspringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//等同于同时加上了@Controller和@ResponseBody
+@RestController
+public class HelloController {
+    //访问/hello或者/hi任何一个地址，都会返回一样的结果
+    @RequestMapping(value = {"/hello","/hi"},method = RequestMethod.GET)
+    public String say(){
+        return "hi you!!!";
+    }
+}
