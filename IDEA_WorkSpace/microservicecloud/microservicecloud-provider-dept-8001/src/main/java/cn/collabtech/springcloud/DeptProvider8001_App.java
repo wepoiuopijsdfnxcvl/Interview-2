be2da5fd1e1ae936255caf9e7e16402805036510
DarrickAZ <zhwwhx@126.com>
2019-01-24 14:12:50 +0800

    ###update.

--- IDEA_WorkSpace/microservicecloud/microservicecloud-provider-dept-8001/src/main/java/cn/collabtech/springcloud/DeptProvider8001_App.java
+++ IDEA_WorkSpace/microservicecloud/microservicecloud-provider-dept-8001/src/main/java/cn/collabtech/springcloud/DeptProvider8001_App.java
@@ -2,12 +2,13 @@ package cn.collabtech.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @SpringBootApplication
-public class DeptProvider8001_App
-{
-  public static void main(String[] args)
-  {
-   SpringApplication.run(DeptProvider8001_App.class, args);
-  }
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+public class DeptProvider8001_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider8001_App.class, args);
+    }
 }
